4e8b80e1980e79042e89a9fc17e7a4c03804fc72
omaralhlawany <OmarAlhlawany>
2024-08-03 13:42:37 +0300

    Establishing Users List Viewer

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+// app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { SearchComponent } from './search/search.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserService } from './user.service';
+import { User } from './user.model';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './hero/header.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, SearchComponent, UserListComponent],
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'user-cards-app';
+  filteredUsers: User[] = []; // Ensure this is properly initialized
+  constructor(private userService: UserService) {}
+  ngOnInit(): void {
+    this.userService.users$.subscribe(users => {
+      this.filteredUsers = users;
+    });
+  }
+  onSearch(term: string): void {
+    this.userService.searchUsers(term);
+  }
+}
